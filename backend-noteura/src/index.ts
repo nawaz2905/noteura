ff6c373ee97951f0795b6d47b81c5868c2d3a85c
Nawaz Khan <abunawaz2905@gmail.com>
2026-01-09 20:35:06 +0530

    backend updated

--- backend-noteura/src/index.ts
+++ backend-noteura/src/index.ts
@@ -1,18 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+
 import './config.js'
 import './db.js'
-import cors from 'cors'
 import {userRouter} from './routes/user.js'
 
 
-import express from 'express'
-
 const app = express()
-app.use(cors())
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 app.use("/api/v1",userRouter)
-app.get("/health", (req, res) => {
-  res.send("Backend is working ✅");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
 
 export default app;
