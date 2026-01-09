c3108535040d1c1ad6fadfe0f44a6aaeaaf0d42f
Nawaz Khan <abunawaz2905@gmail.com>
2026-01-09 23:53:17 +0530

    backend url set in frontend

--- frontend-noteura/src/config.ts
+++ frontend-noteura/src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://backend-noteura-1.onrender.com"
 export const SITE_URL = window.location.origin;
